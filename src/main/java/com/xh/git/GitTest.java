dc82bffd3d6363f04c2c7c4ac33af9f27a0e632a
xmx <battleoutside@gmail.com>
2022-04-21 15:29:29 +0800

    first commit

---
+++ src/main/java/com/xh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xh.git;
+
+/**
+ * @description:
+ * @author: 潇洒的大队长
+ * @create: 2022-04-21 15:22
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
