d9c53f746eea3cf88e82ab17269cfddc13b539e5
熙 <51222624+battleoutside@users.noreply.github.com>
2022-04-21 17:15:02 +0800

    pull commit

--- src/main/java/com/xh/git/GitTest.java
+++ src/main/java/com/xh/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git!3");
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix2");
+        System.out.println("pull commit");
     }
 }
